178fb17c1a9f30342bf3ff3c0fcd37b36b8fe830
hiryuken <alessandro.diaco@gmail.com>
2014-11-25 11:34:21 +0100

    Meteo Hiryuken
    Meteo Application on Hiryuken

---
+++ search.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Condizioni atmosferiche per: <?=$query?></title>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body background="basic.jpg">
+
+	<div style="float: right;clear: all;">
+		<h4>Ricerca Meteo</h4>
+		<form method="get" action="index.php">
+			<input type="text" name="location">
+			<input type="submit" value="Cerca">
+		</form>
+	</div>
+
+</html>
+
+
+		
